ce87240b525933b4a8c4369471a0db0ead1fe612
Mihir Sai Dudekula <mihirsaidudekula@gmail.com>
2023-02-28 12:27:12 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Signin from "./signin.jsx";
+import Navbar from "./navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Signin />
     </>
   );

832890f8e1578bc298160147f9ecfed64fcd4eef
Mihir Sai Dudekula <mihirsaidudekula@gmail.com>
2023-02-28 13:25:06 +0000

    Update 3 files

--- src/signin.jsx
+++ src/signin.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
 
 function Signin() {
+  const [usermail, setUsermail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -32,6 +35,8 @@ function Signin() {
                   type="email"
                   name="email"
                   id="email"
+                  value={usermail}
+                  onChange={(ev) => setUsermail(ev.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
@@ -49,6 +54,8 @@ function Signin() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
+                  value={password}
+                  onChange={(ev) => setPassword(ev.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
